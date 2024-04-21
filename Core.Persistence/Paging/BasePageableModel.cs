75e8086dcc40ab47e68a64204234f32845b35b5e
Melih DİNÇER <m_dincer41@hotmail.com>
2024-04-21 21:13:30 +0300

    Sayfalama işlemlerinin yapılması.

---
+++ Core.Persistence/Paging/BasePageableModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Persistence.Paging;
+
+public abstract class BasePageableModel
+{
+    public int Size { get; set; } //Sayfadaki data sayısı
+    public int Index { get; set; } //Kaçıncı sayfa olduğumuz
+    public int Count { get; set; } //Kaç veri olduğu
+    public int Pages { get; set; } //Toplam kaç sayfamızın olduğu
+    public bool HasPrevious { get; set; }
+    public bool HasNext { get; set; }
+}
