7f85443d439958e1591c4328b1f8da11bb882658
Melih DİNÇER <m_dincer41@hotmail.com>
2024-04-28 18:35:53 +0300

    Global Hata Yönetimi implementasyonu

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+
+//Gelecek olan hataları handle edecek yer. İmplementasyon burada değil bu sınıfı implemente eden sınıfta gerçekleştirilecek.
+public abstract class ExceptionHandler
+{
+    public Task HandleExceptionAsync(Exception exception) =>
+        exception switch
+        {
+            BusinessException businessException => HandleException(businessException), //Gelen exception BusinessException türünde ise
+            _ => HandleException(exception) //Değil ise (diğer durumlar için)
+        };
+
+    protected abstract Task HandleException(BusinessException businessException);
+    protected abstract Task HandleException(Exception exception);
+}
